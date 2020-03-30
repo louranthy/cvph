dc4be982ee27d1d8e80bcb868ecc6b13de4a4823
louranthy+1-at-523777741491 <louranthy@gmail.com>
2020-03-30 08:43:41 +0800

    added facility count for local

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { CovidService } from 'app/service/covid.service';
 import * as _ from "lodash";
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label, SingleDataSet, BaseChartDirective } from 'ng2-charts';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { ChartService } from 'app/service/chart.service';
 @Component({
   selector: 'app-home',
@@ -48,6 +48,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     public foreignNationalCasesData = [];
     public confirmedCasesSex = {};
     public confirmedCasesResidence = {};
+    public confirmedCasesFacility = {};
     public localFemale: Number;
     public localMale: Number;
     public ofwCasesSex = {};
@@ -57,12 +58,12 @@ export class HomeComponent implements OnInit, OnDestroy {
     public pieChartOptions: ChartOptions = {
       responsive: true,
     };
-    public pieChartLabels: Label[] = [['SciFi'], ['Drama'], 'Comedy'];
-    public pieChartData: SingleDataSet = [30, 50, 20];
-    public pieChartType: ChartType.Pie;
-    public pieChartLegend = true;
-    public pieChartPlugins = [];
     
+    public confirmedFacilityDtTrigger: Subject<any> = new Subject();
+    confirmedFacilityDataTable: any;
+    confirmedFacilityDtOptions: any;
+    confirmedFacilityTableData = [];
+    @ViewChild('confirmedFacilityTable', {static: true}) confirmedFacilityTable;
   constructor(private  covidService : CovidService,
     private chartService : ChartService) {
       this.chartService.localGenderUpdated.subscribe( value => {
@@ -79,12 +80,33 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.numberCases = data.length;
       this.confirmedCasesData = data;
       this.confirmedCasesSex = _.groupBy(this.confirmedCasesData, 'sex');
-      this.confirmedCasesResidence = _.groupBy(this.confirmedCasesData, 'metadata[row_data][residence]');
+      this.confirmedCasesResidence = _.groupBy(this.confirmedCasesData, 'metadata.row_data.residence');
+      this.confirmedCasesFacility = _.groupBy(this.confirmedCasesData, 'facility');
+      console.log(this.confirmedCasesFacility);
       this.localFemale = Number(this.confirmedCasesSex['Female'].length);
       this.localMale = Number(this.confirmedCasesSex['Male'].length);
       this.addData(this.localChartType, this.localChartData, [this.localMale, this.localFemale], [this.localMale, this.localFemale], 'Local Statistics ' + this.numberCases);
       this.chartService.localGenderUpdated.next(true);
+
+      var facilities = [];
+      _.forEach(this.confirmedCasesFacility, function(el,index,arr){
+        //console.log( 'facility : ' +index + 'count : ' + el.length);
+        facilities.push({"facility" : index , "count" : el.length})
+      });
+       this.confirmedFacilityDtOptions = {
+         data: facilities,
+         processing: true,
+         columns: [
+           {title: 'Facility', data: 'facility'},
+           {title: 'Count', data: 'count'}
+         ]
+       };
+    
+    }, err => {}, () => {
+      this.confirmedFacilityDataTable = $(this.confirmedFacilityTable.nativeElement);
+      this.confirmedFacilityDataTable.DataTable(this.confirmedFacilityDtOptions)
     });
+
      this.localChartType = ChartType.Pie;
      this.localChartData = {
        labels: [],
@@ -113,87 +135,27 @@ export class HomeComponent implements OnInit, OnDestroy {
       { title: 'Male', imageClass: 'fa fa-circle text-info' },
       { title: 'Female', imageClass: 'fa fa-circle text-danger' }
     ];
-     
-
-      this.hoursChartType = ChartType.Line;
-      this.hoursChartData = {
-        labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-        series: [
-          [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
-          [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
-          [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
-        ]
-      };
-      this.hoursChartOptions = {
-        low: 0,
-        high: 800,
-        showArea: true,
-        height: '245px',
-        axisX: {
-          showGrid: false,
-        },
-        lineSmooth: Chartist.Interpolation.simple({
-          divisor: 3
-        }),
-        showLine: false,
-        showPoint: false,
-      };
-      this.hoursChartResponsive = [
-        ['screen and (max-width: 640px)', {
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
-          }
-        }]
-      ];
-      this.hoursChartLegendItems = [
-        { title: 'Open', imageClass: 'fa fa-circle text-info' },
-        { title: 'Click', imageClass: 'fa fa-circle text-danger' },
-        { title: 'Click Second Time', imageClass: 'fa fa-circle text-warning' }
-      ];
-
-      this.activityChartType = ChartType.Bar;
-      this.activityChartData = {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        series: [
-          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-        ]
-      };
-      this.activityChartOptions = {
-        seriesBarDistance: 10,
-        axisX: {
-          showGrid: false
-        },
-        height: '245px'
-      };
-      this.activityChartResponsive = [
-        ['screen and (max-width: 640px)', {
-          seriesBarDistance: 5,
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
-          }
-        }]
-      ];
-      this.activityChartLegendItems = [
-        { title: 'Tesla Model S', imageClass: 'fa fa-circle text-info' },
-        { title: 'BMW 5 Series', imageClass: 'fa fa-circle text-danger' }
-      ];
-
-
     }
 
      addData(chart, chartData, label, data, title) {
-       console.log(label);
           chartData.labels = label;
           chartData.series = data;
           setTimeout(() => {
             chartData.update;
           }, 10);
   }
+
+  groupByAge(ary, keyFunc) {
+    var r = {};
+    ary.forEach(function(x) {
+      var y = keyFunc(x);
+      r[y] = (r[y] || []).concat(x);
+    });
+    return Object.keys(r).map(function(y) {
+      return r[y];
+    });
+  }
+
 
   ngOnDestroy(){
     this.chartService.localGenderUpdated.next(false);
